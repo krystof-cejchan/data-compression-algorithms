745c89a49e46251c33a605af6371682ae6714db8
TheKrystof701 <krystofcejchan@seznam.cz>
2023-11-29 15:16:16 +0100

    file size feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import os.path
+import tkinter
 import tkinter as tk
 from tkinter import filedialog
-import gzip
-import lzma
 
 
 class FileCompressorApp:
@@ -12,15 +12,19 @@ class FileCompressorApp:
         # Create GUI elements
         self.label = tk.Label(root, text="Choose a file to compress:")
         self.label.pack(pady=10)
+        self.label_size = tk.Label(root, text="")
+        self.label_size.pack(pady=10)
 
         self.choose_button = tk.Button(root, text="Choose File", command=self.choose_file)
         self.choose_button.pack(pady=10)
         self.compression_var = tk.StringVar()
         self.compression_var.set('H')  # Default compression algorithm
-        self.gzip_radio = tk.Radiobutton(root, text="Huffman", variable=self.compression_var, value='H')
-        self.gzip_radio.pack(pady=5)
-        self.lzma_radio = tk.Radiobutton(root, text="LZMA", variable=self.compression_var, value="LZMA")
-        self.lzma_radio.pack(pady=5)
+        self.radio = tk.Radiobutton(root, text="Huffman", variable=self.compression_var, value='H')
+        self.radio.pack(pady=5)
+        self.radio = tk.Radiobutton(root, text="LL77", variable=self.compression_var, value="LL77")
+        self.radio.pack(pady=5)
+        self.radio = tk.Radiobutton(root, text="LL78", variable=self.compression_var, value="LL78")
+        self.radio.pack(pady=5)
         self.compress_button = tk.Button(root, text="Compress", command=self.compress_file)
         self.compress_button.pack(pady=10)
 
@@ -28,29 +32,26 @@ class FileCompressorApp:
         file_path = filedialog.askopenfilename()
         self.file_path = file_path
         self.label.config(text=f"Selected file: {file_path}")
+        self.label_size.config(text=f"File size (kB): {round(os.path.getsize(file_path) / 1000)}")
 
     def compress_file(self):
         if not hasattr(self, 'file_path'):
             tk.messagebox.showwarning("Error", "Please choose a file first.")
             return
 
-        compression_options = [
-            ("Gzip", ".gz", gzip.compress),
-            ("LZMA", ".xz", lzma.compress),
-            # Add more compression algorithms as needed
-        ]
+        print(self.compression_var.get())
 
-        file_path = self.file_path
-        for option in compression_options:
-            compression_name, extension, compression_function = option
-            compressed_file_path = file_path + extension
-
-            with open(file_path, 'rb') as f_in, open(compressed_file_path, 'wb') as f_out:
-                compressed_data = compression_function(f_in.read())
-                f_out.write(compressed_data)
-
-            tk.messagebox.showinfo("Success", f"File compressed using {compression_name}. \n"
-                                              f"Compressed file saved as: {compressed_file_path}")
+        # file_path = self.file_path
+        # for option in compression_options:
+        #     compression_name, extension, compression_function = option
+        #     compressed_file_path = file_path + extension
+        #
+        #     with open(file_path, 'rb') as f_in, open(compressed_file_path, 'wb') as f_out:
+        #         compressed_data = compression_function(f_in.read())
+        #         f_out.write(compressed_data)
+        #
+        #     tk.messagebox.showinfo("Success", f"File compressed using {compression_name}. \n"
+        #                                       f"Compressed file saved as: {compressed_file_path}")
 
 
 if __name__ == "__main__":
